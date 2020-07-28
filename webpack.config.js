1dfdce82a231ed2d66087607edb13553fbc78b18
Gianni Moschini <gianni.moschini+github@gmail.com>
2020-07-28 15:33:13 +0100

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    library: '',
+    libraryTarget: 'commonjs2',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+};
