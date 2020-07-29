98e5f8cf4731c0d7c776870b5073b9de31518e69
Gianni Moschini <gianni.moschini+github@gmail.com>
2020-07-29 11:59:08 +0100

    Add babel/plugin-transform-runtime to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,10 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: [
+              '@babel/plugin-transform-runtime'
+            ]
           }
         }
       }
