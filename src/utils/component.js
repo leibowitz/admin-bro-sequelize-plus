9c89ca98616c71e53cad1d730019e4fd07fce5d8
Gianni Moschini <gianni.moschini+github@gmail.com>
2020-09-03 15:54:09 +0100

    Avoid redifining components

--- src/utils/component.js
+++ src/utils/component.js
@@ -1,6 +1,15 @@
 const AdminBro = require('admin-bro')
 const { buildFeature } = require('admin-bro')
 
+const Components = {}
+
+const getComponentId = (type) => {
+  if (!Components[type]) {
+    Components[type] = AdminBro.bundle(`../src/components/${type}.edit.jsx`)
+  }
+  return Components[type]
+}
+
 const component = (type, componentId) => {
   return {
     type,
@@ -11,7 +20,7 @@ const component = (type, componentId) => {
 }
 
 const getComponent = (type) => {
-  const componentId = AdminBro.bundle(`../src/components/${type}.edit.jsx`)
+  const componentId = getComponentId(type)
   return component(type, componentId)
 }
 
